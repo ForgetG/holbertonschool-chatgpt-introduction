67efe2a71e4425296ecea1524d888f5f7827e961
Forget Guillaume <9430@holbertonstudents.com>
2024-09-02 11:30:54 +0200

    added chat gpt doc

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+# This line is called a shebang. It indicates that the script should be run using the Python 3 interpreter.
+
 import sys
+# The 'sys' module provides access to some variables and functions that interact with the Python runtime environment.
+# Specifically, 'sys.argv' will be used to retrieve command-line arguments.
 
 def factorial(n):
-    """
-    Calculate the factorial of a number using recursion.
+    # This function calculates the factorial of a given number 'n' using recursion.
+    # A factorial is the product of an integer and all the integers below it, e.g., factorial(5) = 5 * 4 * 3 * 2 * 1.
 
-    Parameters:
-    n (int): The number to calculate the factorial of. Must be a non-negative integer.
-
-    Returns:
-    int: The factorial of the given number. If n is 0, returns 1.
-    """
     if n == 0:
         return 1
+        # The base case: factorial(0) is defined to be 1.
     else:
         return n * factorial(n-1)
+        # The recursive case: factorial(n) = n * factorial(n-1).
+        # This keeps calling itself with smaller values of 'n' until it reaches the base case.
 
+# The following line retrieves the first command-line argument provided by the user and converts it to an integer.
+# This value is then passed to the 'factorial' function to compute the factorial of that number.
 f = factorial(int(sys.argv[1]))
+
+# Finally, the result of the factorial calculation is printed to the console.
 print(f)
